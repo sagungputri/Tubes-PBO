cce879d28e3aaf23c0ef03a579f8b8d8db0de78f
Anak Agung Sagung Putri Wijayanti <138758788+sagungputri@users.noreply.github.com>
2025-01-04 19:53:13 +0700

    ulang up

---
+++ MedWare_PBO/MedWare_PBO/src/java/models/Person.java
@@ -0,0 +1,19 @@
+package models;
+
+public abstract class Person {
+    protected String username;
+    protected String role;
+
+    public Person(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
